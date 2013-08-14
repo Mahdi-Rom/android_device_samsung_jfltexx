01d1aaf074cf84b98a0b8fad8cdb535bc0511bb2
Mahdi-Rom <mahdi.rom@gmail.com>
2013-08-14 02:20:00 +0100

    mahdi.mk: some changes

--- mahdi.mk
+++ mahdi.mk
@@ -1,17 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := GT-I9505
+
+# Inherit AOSP device configuration.
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Inherit some common config.
-$(call inherit-product, vendor/mahdi/config/common.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/configs/common_full_phone.mk)
 
-# Inherit some common phone config.
-$(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
-$(call inherit-product, vendor/mahdi/config/gsm.mk)
+# Inherit GSM files.
+$(call inherit-product, vendor/mahdi/configs/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/mahdi/config/nfc_enhanced.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jflte BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
-
-PRODUCT_DEVICE := jfltexx
+# Setup device specific product configuration.
 PRODUCT_NAME := mahdi_jfltexx
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := jfltexx
+PRODUCT_MODEL := GT-I9505
+PRODUCT_MANUFACTURER := samsung
 
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
